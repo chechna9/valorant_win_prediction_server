7597598f46da28febb1cb7a87b21b81bde4e3af0
chechna9 <ja_bouchouareb@esi.dz>
2023-05-27 03:08:14 +0100

    server working locally

--- app.py
+++ app.py
@@ -1,21 +1,24 @@
 from flask import Flask, request, jsonify
 from model_dep import *
+from flask_cors import CORS
 import pickle
 
 app = Flask(__name__)
-loaded_model = pickle.load(open("./log_reg_model.pkl", "rb"))
+cors = CORS(app, resources={r"*": {"origins": "*"}})
+loaded_model = pickle.load(open("./finalized_model.sav", 'rb'))
 @app.route('/', methods=['GET'])
 def home():
     return '''<h1>Valorant Prediction API</h1>'''
+
+
 @app.route('/predict', methods=['POST'])
 def predict():
     # Retrieve the input data from the request
     data = request.json['data']
-    
     # Perform prediction using your logistic regression model
-    prediction = loaded_model.predict([data])[0]
+    prediction = loaded_model.predict_proba(data)
     
     # Return the prediction as a JSON response
-    return jsonify({'prediction': prediction})
+    return jsonify({'prediction': prediction[0][1]})
 if __name__ == '__main__':
     app.run(debug=True)
--- model_dep.py
+++ model_dep.py
@@ -1,10 +1,15 @@
+# import pandas as pd
+# from sklearn.model_selection import GridSearchCV,KFold
+# # from sklearn.pipeline import Pipeline,FunctionTransformer
+# # from sklearn.linear_model import LogisticRegression
+# kfold = KFold(n_splits=5,shuffle=True,random_state=12)
+import numpy as np
 import pandas as pd
-from sklearn.model_selection import GridSearchCV,KFold
-from sklearn.pipeline import Pipeline,FunctionTransformer
-from sklearn.linear_model import LogisticRegression
-kfold = KFold(n_splits=5,shuffle=True,random_state=12)
-def m2_feature_selection(X:pd.DataFrame):
+def m2_feature_selection(x:dict):
   features = ['t1_duelist', 't1_sentinel', 't1_initiator', 't1_controller',
               't2_duelist', 't2_sentinel', 't2_initiator', 't2_controller','Map',
               'Team1_SideFirstHalf']
-  return X[features]
+  df = pd.DataFrame.from_dict(x,orient='index', columns=['values']).transpose()
+
+ 
+  return df[features]
